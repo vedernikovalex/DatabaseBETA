4e0e3c92ae76346adfa5ae41d187f77a752d9eb0
unknown <vedurnikof@gmail.com>
2023-01-30 11:12:08 -0800

    database connection v0.05

--- DatabaseBETA/Form1.Designer.cs
+++ DatabaseBETA/Form1.Designer.cs
@@ -31,6 +31,12 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.usernameBox = new System.Windows.Forms.TextBox();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.guestButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,27 +44,89 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(580, 39);
+            this.label1.Location = new System.Drawing.Point(332, -22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(300, 125);
+            this.label1.Size = new System.Drawing.Size(761, 125);
             this.label1.TabIndex = 0;
-            this.label1.Text = "LOGO";
+            this.label1.Text = "AUTOMECHANIK";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(396, 179);
+            this.pictureBox1.Location = new System.Drawing.Point(588, 106);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(693, 399);
+            this.pictureBox1.Size = new System.Drawing.Size(251, 250);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loginButton.Location = new System.Drawing.Point(639, 684);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(143, 94);
+            this.loginButton.TabIndex = 2;
+            this.loginButton.Text = "LOGIN";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // usernameBox
+            // 
+            this.usernameBox.Location = new System.Drawing.Point(535, 512);
+            this.usernameBox.Name = "usernameBox";
+            this.usernameBox.Size = new System.Drawing.Size(352, 35);
+            this.usernameBox.TabIndex = 3;
+            this.usernameBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Location = new System.Drawing.Point(535, 607);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(352, 35);
+            this.passwordBox.TabIndex = 4;
+            this.passwordBox.TextChanged += new System.EventHandler(this.passwordBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(535, 479);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 30);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "USERNAME";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(535, 574);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 30);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "PASSWORD";
+            // 
+            // guestButton
+            // 
+            this.guestButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.guestButton.Location = new System.Drawing.Point(657, 827);
+            this.guestButton.Name = "guestButton";
+            this.guestButton.Size = new System.Drawing.Size(104, 40);
+            this.guestButton.TabIndex = 7;
+            this.guestButton.Text = "GUEST";
+            this.guestButton.UseVisualStyleBackColor = true;
+            this.guestButton.Click += new System.EventHandler(this.guestButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1481, 930);
+            this.Controls.Add(this.guestButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.usernameBox);
+            this.Controls.Add(this.loginButton);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -73,5 +141,11 @@
 
         private Label label1;
         private PictureBox pictureBox1;
+        private Button loginButton;
+        private TextBox usernameBox;
+        private TextBox passwordBox;
+        private Label label2;
+        private Label label3;
+        private Button guestButton;
     }
 }
--- DatabaseBETA/Form1.cs
+++ DatabaseBETA/Form1.cs
@@ -1,7 +1,24 @@
+using System.Data.SqlClient;
+using System.Configuration;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Diagnostics;
+
 namespace DatabaseBETA
 {
     public partial class Form1 : Form
     {
+        private string loginInput;
+        private string passwordInput;
+        private bool loggedIn = false;
+        private SqlCommand command;
+        private SqlDataReader reader;
+        private string sqlCommand, output;
+
+        //private string dataSource = ConfigurationManager.AppSettings.Get("macDBDataSource");
+        //private string databaseName = ConfigurationManager.AppSettings.Get("DBName");
+
+        //
+        private string connectionString = string.Format("Data Source=(LocalDb)\\beta;Integrated Security=true;", ConfigurationManager.AppSettings.Get("macDBDataSource"), ConfigurationManager.AppSettings.Get("DBName"));
         public Form1()
         {
             InitializeComponent();
@@ -9,7 +26,49 @@ namespace DatabaseBETA
 
         private void label1_Click(object sender, EventArgs e)
         {
-            Console.WriteLine("Label 1 clicked");
+            Debug.WriteLine("Label 1 clicked");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            loginInput = usernameBox.Text;
+        }
+
+        private void passwordBox_TextChanged(object sender, EventArgs e)
+        {
+            passwordInput= passwordBox.Text;
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(loginInput) && !String.IsNullOrEmpty(passwordInput))
+            {
+                Debug.WriteLine(loginInput);
+                Debug.WriteLine(passwordInput);
+                loggedIn= true;
+                Debug.WriteLine(loggedIn);
+            }
+        }
+
+        private void guestButton_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine(connectionString);
+            SqlConnection con = new SqlConnection(connectionString);
+            try
+            {
+                con.Open();
+                if (con.State == System.Data.ConnectionState.Open)
+                {
+                    this.Hide();
+                    Form2 form2= new Form2();
+                    form2.ShowDialog();
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            
         }
     }
 }
